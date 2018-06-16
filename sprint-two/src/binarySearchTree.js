0c2fb7d8340b62d7e828eadb667a261fa5d5e744
dyliman <dyyliman@gmail.com>
2018-06-15 23:15:20 -0700

    Finished BinarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,62 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
+
+
+BinarySearchTree.prototype.insert = function(value){
+  if(this.value > value) {
+    if(this.left === null){
+      this.left = new BinarySearchTree(value);
+    } else{
+      this.left.insert(value);
+    }
+
+  } else if (this.value < value) {
+    if(this.right === null){
+      this.right = new BinarySearchTree(value);
+    } else{
+      this.right.insert(value);
+    }
+  } else {
+    return "already in tree";
+  }
+}
+
+BinarySearchTree.prototype.contains = function(target){
+  if(target === this.value) {
+    return true;
+  }
+
+  if(this.value > target && this.left !== null) {
+    return this.left.contains(target);
+  } else if (this.value < target && this.right !== null) {
+    return this.right.contains(target);
+  }
+  
+  return false; 
+}
+
+BinarySearchTree.prototype.depthFirstLog = function(cb){
+  cb(this.value);
+
+  if(this.left !== null){
+    this.left.depthFirstLog(cb);
+  }
+  if(this.right !== null){
+    this.right.depthFirstLog(cb); 
+  }
+}
+
+
+
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+insert:O(n)
+contains:O(log(n))
+depthFirstLog:O(n)
+BinarySearchTree:O(1)
  */
