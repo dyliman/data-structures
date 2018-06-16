e31197594f5f1f2b7fb1e2e482f23b8861bb8caf
dyliman <dyyliman@gmail.com>
2018-06-16 15:01:17 -0700

    Added size function to tree

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -41,4 +41,16 @@ describe('tree', function() {
     expect(tree.contains(8)).to.equal(true);
   });
 
+  it('should return number of items in tree', function() {
+    tree.addChild(5);
+    tree.addChild(6);
+    tree.children[0].addChild(7);
+    tree.children[1].addChild(8);    
+    tree.children[0].addChild(9);
+    tree.children[1].addChild(1);
+    tree.children[0].addChild(2);
+    tree.children[1].addChild(3);
+    expect(tree.size()).to.equal(9);
+  });
+
 });
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,6 +1,7 @@
 var Tree = function(value) {
   var newTree = {};
-  newTree.value = value;
+  newTree.value = value || 0;
+  newTree.count = 1;
 
   // your code here
   newTree.children = [];  // fix me
@@ -18,22 +19,30 @@ treeMethods.addChild = function(value) {
 treeMethods.contains = function(target) {
   if(this.value === target){
     return true;
-  } else if(this.children.length > 0){
-    for(var i = 0; i < this.children.length; i++){
-      if(this.children[i].contains(target)){
-        return true;
-      }
+  } 
+  for(var i = 0; i < this.children.length; i++){
+    if(this.children[i].contains(target)){
+      return true;
     }
   }
   return false;
 };
 
+treeMethods.size = function(){
+  for(var i = 0; i < this.children.length; i++){
+    this.count += this.children[i].size();
+  }
 
+  return this.count;
+};
+
+// how many items are in the tree
 
 /*
  * Complexity: What is the time complexity of the above functions?
 addChild: O(1)
 contains: O(n)
+size: O(n)
 Tree:O(1)
  */
 
